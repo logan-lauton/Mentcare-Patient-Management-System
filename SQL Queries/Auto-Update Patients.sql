d19f0118b18e92c8133ee78ab294e9860cb3a356
Logan Lauton <Logan.lauton@marquette.edu>
2023-04-17 10:07:20 -0500

    Update Auto-Update Patients.sql
    Adding the other query needed to update the PATIENTS Table automatically after a call to the APPOINTMENTS Table alters it in anyway. These queries will be added to the notebook in order for these pieces to happen automatically.

--- SQL Queries/Auto-Update Patients.sql
+++ SQL Queries/Auto-Update Patients.sql
@@ -13,3 +13,17 @@ FROM
     Appointments 
 GROUP BY 
     PATIENTUNAME, PATIENTFNAME, PATIENTLNAME, PATIENTEMAIL, PATIENTMEDS;
+
+--The following query is the update code(python) that will occur in case of a user already 
+--existing in the table. With both of these queries complete, we can now automatically update 
+--both tables when a new appointment is scheduled by users such as admins
+UPDATE PATIENTS p
+SET 
+    LASTATTEND = (
+        SELECT 
+            TO_CHAR(MAX(TO_DATE(a.APPOINTMENTDATE, 'MM/DD/YYYY')), 'MM/DD/YYYY')
+        FROM 
+            Appointments a
+        WHERE 
+            a.PATIENTUNAME = p.USERNAME
+    );
